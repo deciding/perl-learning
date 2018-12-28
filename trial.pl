50a23b0e8a46a67cc5ae9a36162113488268f23b
deciding <zhangzn710@gmail.com>
2018-12-28 23:09:39 +0800

    string arr detailed

--- trial.pl
+++ trial.pl
@@ -33,16 +33,80 @@ print "$str";
 #=================data type===================
 #$var @arr %map
 
-#=================array and dict===================
-@arr=(1,2,3);
-print "\$arr[0]=$arr[0]\n";
+#=================array===================
+#both arr and dict use () and qw// to create
+#but use [] and {} to access
+#both use $ to access
 
+#construct
+@arr=(1,2,3);
+@arr2=qw\1 2 3\;
+@arr3=qw\1
+2
+3\;
+
+print "\$arr[0]=$arr[0]\n";
+print "\$arr[-1]=$arr[-1]\n"; #like python
+print "\$arr2[0]=$arr2[0]\n";
+print "\$arr3[0]=$arr3[0]\n";
+
+#range
+@arr4=(1..20);
+print "@arr4\n";
+
+#copy by value
+@copy=@arr;
+$copy[0]=2;
+print "$copy[0] vs $arr[0]\n";
+
+#size and max index
+$size=@arr;
+$maxindex=$#arr; # careful! use $, since we are getting scalar
+print "size of \@arr: $size\n";
+print "maxindex of \@arr: $maxindex\n";
+
+#push pop shift unshift
+push(@arr, 5);
+unshift(@arr,0);
+print "@arr\n";
+pop(@arr);
+shift(@arr);
+print "@arr\n";
+
+#slicing
+print "@arr4[2,3,4]\n"; #careful! use @ instead of $, since we are getting arr
+print "@arr4[2..4]\n";
+
+#splice -- changing the arr
+splice(@arr4, 10); # replace from 11 to null
+print "@arr4\n";
+splice(@arr4, 5, 2); # replace from 6-7 to null
+print "@arr4\n";
+splice(@arr4, 1, 2, 10..11); # replace from 2-3 to (10,11)
+print "@arr4\n";
+
+#=================dict===================
 %dict=('a',1,'b',2,'c',3);
 print "\$dict{'a'}=$dict{'a'}\n";
 
+#copy by value
 @copy=@arr;
 $copy[0]=2;
 print "$copy[0] vs $arr[0]\n";
 
 $size=@arr;
 print "size of \@arr: $size\n";
+
+#=================string===================
+$str='a' . 15;
+print"$str is a concat string \n";
+
+$multilineStr='aaa
+aaa'."\n";
+$multilineStr2=<<"EOF";
+aaa
+aaa
+EOF
+
+print "$multilineStr";
+print "$multilineStr2";
