d855271e3d47cad1351780e41376b4e3d5d0a947
deciding <zhangzn710@gmail.com>
2018-12-30 00:14:05 +0800

    added condition and loop controls

--- trial.pl
+++ trial.pl
@@ -152,3 +152,152 @@ EOF
 
 print "$multilineStr";
 print "$multilineStr2";
+
+#=================condition===================
+$a=100;
+if($a==10){
+    print "\$a=10: $a\n";
+}
+elsif($a==50){
+    print "\$a=50: $a\n";
+}
+else{
+    print "\$a=100: $a\n";
+}
+
+$b=100;
+unless($a==100){
+    print "as expected, we didn't get 100: $a\n";
+}
+elsif($b==100){
+    print "we got a == 100 unexpectedly, in this case we expect b=100 also: $a, $b\n";
+}
+else {
+    print "very strange situation: a==100 but b!=100\n";
+} 
+
+use Switch;
+$var=10;
+@arr=(1,2,10);
+%hash=('a'=>5,10=>'a');
+switch($var){
+    case 11		{print "\$var is 11\n";}#careful the ;
+    case [1..6,9]	{print "\$var is in list\n";}#careful the [] expression in case, instead of ()
+    case (\@arr)	{print "\$var is in \@arr\n"; next;}#(\) syntax, and next , since break is the default behaviour
+    case (\%hash)	{print "\$var is in \%hash\n";}
+    else		{print "\$var is not the above cases\n";}#use else instead of default
+}
+
+$tri= 1==2? 'a':'b';
+print "$tri\n";
+
+
+#=================loop===================
+#while loop
+$a = 10;
+while( $a < 20 ) {
+   printf "Value of a: $a\n";
+   $a = $a + 1;
+}
+$a = 10;
+do{
+   printf "Value of a: $a\n";
+   $a = $a + 1;
+}while( $a < 20 );
+
+#until loop
+$a = 5;
+until( $a > 10 ) {
+   printf "Value of a: $a\n";
+   $a = $a + 1;
+}
+
+#for loop
+for( $a = 10; $a < 20; $a = $a + 1 ) {
+   print "value of a: $a\n";
+}
+
+#foreach list
+@list = (2, 20, 30, 40, 50);
+foreach $a (@list) {
+   print "value of a: $a\n";
+}
+
+#next statement: continue
+$a = 0;
+OUTER: while( $a < 4 ) {
+   $b = 0;
+   print "value of a: $a\n";
+   INNER:while ( $b < 4) {
+      if( $a == 2) {
+         $a = $a + 1;
+         # jump to outer loop
+         next OUTER;
+      }
+      $b = $b + 1;
+      print "Value of b : $b\n";
+   }
+   print "\n";
+   $a = $a + 1;
+}
+
+#last statement: break
+$a = 0;
+OUTER: while( $a < 4 ) {
+   $b = 0;
+   print "value of a: $a\n";
+   INNER:while ( $b < 4) {
+      if( $a == 2) {
+         # terminate outer loop
+         last OUTER;
+      }
+      $b = $b + 1;
+      print "Value of b : $b\n";
+   }
+   print "\n";
+   $a = $a + 1;
+}
+
+#continue block: executed just befor condition evaluation, can only be used in foreach and while
+$a = 0;
+while($a < 3) {
+   print "Value of a = $a\n";
+} continue {
+   $a = $a + 1;
+}
+@list = (1, 2, 3, 4, 5);
+foreach $a (@list) {
+   print "Value of a = $a\n";
+} continue {
+   last if $a == 4; # last if statement
+}
+
+#redo statement: restart the loop without condition evaluation
+$a = 0;
+while($a < 10) {
+   if( $a == 5 ) {
+      $a = $a + 1;
+      redo;
+   }
+   print "Value of a = $a\n";
+} continue {
+   $a = $a + 1;
+}
+
+#goto statement
+$a = 10;
+$str1 = "LO";
+$str2 = "OP";
+LOOP:do {
+   if( $a == 15) {
+      # skip the iteration.
+      $a = $a + 1;
+      # use goto EXPR form
+      goto $str1.$str2;
+   }
+   print "Value of a = $a\n";
+   $a = $a + 1;
+} while( $a < 20 );
+
+
+
